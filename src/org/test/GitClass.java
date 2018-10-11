eef67daedd11b925438c50e07cf57523c409eb38
madhuri <maduri0212@gmail.com>
2018-10-11 13:12:00 +0530

    updated hello method

--- src/org/test/GitClass.java
+++ src/org/test/GitClass.java
@@ -4,11 +4,11 @@ public class GitClass {
 
 	public static void main(String[] args) {
 		System.out.println("first git code");
-		
-	}
-	public void hello() {
-		// TODO Auto-generated method stub
 
+	}
+
+	public void hello() {
+		System.out.println("updated code");
 	}
 
 }
